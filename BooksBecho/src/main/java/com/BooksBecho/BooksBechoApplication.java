481cb1c62422d4ec391081fc91fa0d2ee7dbd380
Admin <Admin@LAPTOP-1932E2UT>
2020-03-03 23:30:38 +0530

    First GitHub commit

---
+++ BooksBecho/src/main/java/com/BooksBecho/BooksBechoApplication.java
@@ -0,0 +1,13 @@
+package com.BooksBecho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksBechoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksBechoApplication.class, args);
+	}
+
+}
---
+++ BooksBecho/src/test/java/com/BooksBecho/BooksBechoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BooksBecho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksBechoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
